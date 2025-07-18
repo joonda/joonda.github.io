24132e361b0ea15871d6fc1a3986f4e114c60253
joonda <kloudzero@kakao.com>
2025-07-18 20:42:51 +0900

    :sparkles: [Feat] about page 초안

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,27 @@
 export default function About() {
     return (
-        <div>About pages</div>
+        <div className="max-w-3xl mx-auto">
+            <div className="px-4 py-8 mt-4 flex flex-col gap-1">
+                <p className="text-2xl py-4 font-bold">About</p>
+                <p>안녕하세요 개발자로 일하고 있는 이현준 입니다.</p>
+                <p>생각나면 끄적이거나 정리하는 것을 좋아하며, 항상 Why를 많이 생각하는 개발자 입니다.</p>
+                {/*github, mail, linkedin icon 추가*/}
+            </div>
+            <div className="p-4 flex flex-col gap-1">
+                {/* Work Experience area */}
+            </div>
+            <div className="p-4 flex flex-col gap-1">
+                {/* Education */}
+            </div>
+            <div className="p-4 flex flex-col gap-1">
+                {/* Skills */}
+            </div>
+            <div>
+                {/* Project */}
+            </div>
+            <div>
+                {/* Certificates */}
+            </div>
+        </div>
     )
 }
