ebe0ed5d6223a4887125ac8174422891ed7280a0
joonda <kloudzero@kakao.com>
2025-07-26 02:41:19 +0900

    :bug: [Fix] 비동기 수정

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -1,20 +1,18 @@
 // app/blog/[category]/page.tsx
-import {getSortedPostList, getCategoryList} from "@/lib/posts";
+import { getSortedPostList, getCategoryList } from "@/lib/posts";
 import MainPage from "@/components/mainpage";
 
 interface CategoryPageProps {
-  params: { category: string };
+  params: Promise<{ category: string }>;  // Promise로 변경
 }
 
-export function generateStaticParams(): {category: string}[] {
+export function generateStaticParams() {
   const categoryList = getCategoryList();
-  const paramList = categoryList.map(category => ({category}))
-  return paramList
+  return categoryList.map(category => ({ category }));
 }
 
-export default async function CategoryPage( { params }: CategoryPageProps) {
-
-  const postList = await getSortedPostList(params.category); // getPostList 함수에서 category로 필터링된 결과를 가져옴
-
-  return <MainPage postList={postList} category={params.category} />;
+export default async function CategoryPage({ params }: CategoryPageProps) {
+  const { category } = await params;  // params를 await로 처리
+  const postList = await getSortedPostList(category);
+  return <MainPage postList={postList} category={category} />;
 }
